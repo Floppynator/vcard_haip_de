128357c7cd6b5b57df3bb10209bee0d168212254
Christian Haip <haip@denic-services.de>
2021-06-07 09:23:08 +0200

    running state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <React.StrictMode>
-    <App />
+    <div className="max-h-screen h-screen flex justify-center items-center bg-gray-100">
+      <div className="font-semibold text-5xl font-mono flex flex-col">
+        Hello World.
+        <span className="text-sm text-gray-500">
+          This is the code you're looking for.
+        </span>
+      </div>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
